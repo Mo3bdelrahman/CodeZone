e4aac7e843836bba506781a3da626cd581cdf68b
Mohamed Abdelrahman <mohamedbdelrahman2@gmail.com>
2024-07-10 19:29:08 +0300

    Add project files.

---
+++ CodeZone.Application/ApplicationSerivceRegistration.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeZone.Application
+{
+    public static class ApplicationSerivceRegistration
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            // add mappers,middelwares and services
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            return services;
+
+        }
+    }
+}
---
+++ CodeZone.Persistence/PersistenceServiceResgistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace CodeZone.Persistence
+{
+    public static class PersistenceServiceResgistration
+    {
+        public static IServiceCollection AddPersistanceService(this IServiceCollection services, IConfiguration configuration)
+        {
+            //add dbcontext and repositories 
+
+            return services;
+        }
+    }
+}
